fadae7cd32c510bedf1a941956b101c6aae3694e
fxbite <letruongthinh1703@gmail.com>
2022-06-11 09:29:15 +0700

    Using passport

---
+++ src/config/passport.ts
@@ -0,0 +1,18 @@
+import { Strategy } from 'passport-google-oauth20';
+import mongoose from "mongoose";
+import {PassportStatic} from 'passport'
+
+const GoogleStrategy = Strategy
+
+export const googlePassport = (passport: PassportStatic) => {
+    passport.use(new GoogleStrategy({
+        clientID: process.env.CLIENT_ID!,
+        clientSecret: process.env.CLIENT_SECRET!,
+        callbackURL: '/auth/google/callback'
+    },
+    async(accessToken, refreshToken, profile, done) => {
+
+    }))
+}
+
+
